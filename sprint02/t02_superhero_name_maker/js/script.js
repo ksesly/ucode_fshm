6b870588fd7be11c5b58df7bfebfa40a0c7957c9
ksesly <ks.you.04@gmail.com>
2023-05-21 19:21:02 +0300

    new: t02

---
+++ sprint02/t02_superhero_name_maker/js/script.js
@@ -0,0 +1,74 @@
+let animal;
+let gender;
+let age;
+
+function animalInput () {
+    animal = prompt('What animal is the superhero most similar to?', '');
+    console.log(animal);
+    let animalRegex = RegExp('^[a-zA-Z]{1,20}$').test(animal);
+    if (animalRegex) 
+        return;
+    else {
+        alert('error: length less than 20\nonly one word that contains only letters');
+        animal = null;
+    }
+}
+
+function genderInput () {
+    gender = prompt('Is the superhero male or female? Leave blank if unknown or other', '');
+    let genderRegex = RegExp('^(female|male|)$', 'i').test(gender);
+    if (genderRegex) 
+        return
+    else {
+        alert('error: accepts onlymale,female, or blank');
+        gender = null;
+    }
+}
+
+function ageInput () {
+    age = prompt('How old is the superhero?', '');
+    let ageRegex = RegExp(`^(?!0)\\d{1,4}$`).test(age);
+    if (ageRegex) 
+        return;
+    else {
+        alert('error: length <= 5, only digits, cannot start with a zero');
+        age = 0;
+    }
+}
+
+
+while (animal == null) {
+    animalInput();
+}
+while (gender == null) {
+    genderInput();
+}
+while (age == null) {
+    ageInput();
+}
+
+
+if  (RegExp('^male', 'i').test(gender)) {
+    if(age < 19) 
+        alert(`${animal}-boy!`);
+    else 
+        alert(`${animal}-man!`);
+}
+else if (RegExp('^female', 'i').test(gender)) {
+    if (age < 19)
+        alert(`${animal}-girl!`);
+    else
+        alert(`${animal}-woman!`);
+}
+else if (RegExp('^$', 'i').test(gender)) {
+    if (age < 19)
+        alert(`${animal}-kid!`);
+    else
+        alert(`${animal}-hero!`);
+}
+
+
+
+
+
+
