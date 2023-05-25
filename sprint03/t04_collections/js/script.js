0228eb878556745b19591373ed1305168cd58b0a
ksesly <ks.you.04@gmail.com>
2023-05-26 01:15:10 +0300

    fix: t04

--- sprint03/t04_collections/js/script.js
+++ sprint03/t04_collections/js/script.js
@@ -1,52 +1,122 @@
+/*guestList
+–contains names of invited guests
+–if you give it your name, it will tell
+     you if you're on the list
+–if you ask who, or how many people are 
+    invited, it will not tell you
+–you can ask to remove someone by name off 
+    the list, but not everyone at once 
+*/
 
+const guestList = new WeakSet();
+
+let guest1 = { name: 'miska'};
+let guest2 = { name: 'katjushka'};
+let guest3 = { name: 'tigr'};
  
+guestList.add(guest1);
+guestList.add(guest2);
+guestList.add(guest3);
+
+console.log(guestList.has(guest1))
+
+console.log({name: 'tigr'});
+
+console.log(guestList);
+console.log(guestList.size);
+
+guestList.delete({name: 'tigr'});
+console.log(guestList.has({name: 'tigr'}));
 
 
+/* menu
+–contains a list of unique dishes
+    and their prices
+–you can ask to name every available 
+dish and its price, one after the othe 
+*/
+
+const menu = new Map();
+
+menu.set('pasta', '5');
+menu.set('water', '74');
+menu.set('salad', '46');
+menu.set('porige', '120');
+menu.set('falafel', '30');
+
+menu.forEach((value, key) => {
+    console.log(key, value);
+})
+
+console.log(menu.has('pasta'));
+console.log(menu.has('juice'));
+
+menu.delete('porige');
+console.log(menu.has('porige'));
+
+console.log(menu.size);
 
 
+/* bankVault
+–contains safety deposit boxes, 
+    each with unique credentials and some
+    contents
+–the only way to see the contents 
+    of a box, is to provide its correct credentials 
+*/
+
+const bankVault = new WeakMap();
+
+const safe1 = {};
+const safe2 = {};
+const safe3 = {};
+const safe4 = {};
+
+bankVault.set(safe1, ['credentials1', 'juweleries']);
+bankVault.set(safe2, ['credentials2', 'money']);
+bankVault.set(safe3, ['credentials3', 'food']);
+bankVault.set(safe4, ['credentials4', 'books']);
+
+console.log(bankVault.get(safe2, 'credentials2'));
 
 
+const enteredCred  = safe1;
+if (bankVault.has(enteredCred)) {
+    const contents = bankVault.get(enteredCred);
+    console.log(`Box contents: ${contents}`);
+} else {
+    console.log("Invalid credentials.");
+}
 
 
-
-
-
-
-
-// const guestList = new WeakMap();
-
-// function isGuestInvited(guest) {
-//     return guestList.has(guest);
+// const enteredCred1  = safe36;
+// if (bankVault.has(enteredCred1)) {
+//     const contents = bankVault.get(enteredCred1);
+//     console.log(`Box contents: ${contents}`);
+// } else {
+//     console.log("Invalid credentials.");
 // }
-  
-// function inviteGuest(guest) {
-//     guestList.set(guest, true);
-// }
-
-// function removeGuest(guest) {
-//     guestList.delete(guest);
-// }
 
 
-// const guest1 = { name: 'mishka' };
-// const guest2 = { name: 'katjushka' };
-// const guest3 = { name: 'kolyusik' }
+/* coinCollection
+–contains various coins, all unique
+–if you want, you can see the entire collection
+*/
 
+const coinCollection = new Set();
 
-// inviteGuest(guest1);
-// inviteGuest(guest2);
-// inviteGuest(guest3);
+coinCollection.add('old coin');
+coinCollection.add('ancient coin');
+coinCollection.add('jungle coin');
+coinCollection.add('marine coin');
+coinCollection.add('air coin');
 
+console.log(coinCollection.has('ancient coin')); 
 
-// console.log(guestList.has(guest1)); 
-// true
+coinCollection.delete('air coin');
 
-// console.log(isGuestInvited({ name: 'Bob' })); 
-// false
-
-// removeGuest({name: 'kolyusik'});
-
-// console.log(guestList);
+console.log(coinCollection);
+console.log(coinCollection.size);
 
 
 
