f3f60a163f8f3f110da47c86b5690f61fb508100
ksesly <ks.you.04@gmail.com>
2023-05-25 18:33:29 +0300

    new: t05

---
+++ sprint03/t04_collections/js/script.js
@@ -0,0 +1,85 @@
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const guestList = new WeakMap();
+
+// function isGuestInvited(guest) {
+//     return guestList.has(guest);
+// }
+  
+// function inviteGuest(guest) {
+//     guestList.set(guest, true);
+// }
+
+// function removeGuest(guest) {
+//     guestList.delete(guest);
+// }
+
+
+// const guest1 = { name: 'mishka' };
+// const guest2 = { name: 'katjushka' };
+// const guest3 = { name: 'kolyusik' }
+
+
+// inviteGuest(guest1);
+// inviteGuest(guest2);
+// inviteGuest(guest3);
+
+
+// console.log(guestList.has(guest1)); 
+// true
+
+// console.log(isGuestInvited({ name: 'Bob' })); 
+// false
+
+// removeGuest({name: 'kolyusik'});
+
+// console.log(guestList);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const coinCollection = new Set();
+
+// coinCollection.add('mishka');
+// coinCollection.add('katjushka');
+// coinCollection.add('davidushka');
+// coinCollection.add('humus');
+
+// console.log(coinCollection);
+// // [ "mishka", "katjushka", "davidushka", "humus" ]
+
+// coinCollection.delete('davidushka');
+// console.log(coinCollection);
+// // [ "mishka", "katjushka", "humus" ]
+
+// for (const value of coinCollection) console.log(value);
+// // mishka 
+// // katjushka 
+// // humus
+
+// console.log(coinCollection.has('mishka'));
+// // true
+
+
