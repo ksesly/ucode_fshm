bafdf1ef25486926d09a8b91de735617b3209bd3
ksesly <ks.you.04@gmail.com>
2023-05-26 23:43:05 +0300

    new: t11 strat

---
+++ sprint03/t11_upgrade_human/js/script.js
@@ -0,0 +1,17 @@
+class Human {
+    constructor(firstName, lastName, gender, age, calories) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.age = age; 
+        this.calories = calories;
+    }
+
+    sleepFor() {
+
+    }
+
+    feed() {
+        
+    }
+}
