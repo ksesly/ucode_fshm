c44f17041657b0f408234129bd6ec801d05db0f0
ksesly <ks.you.04@gmail.com>
2023-05-30 09:54:26 +0300

    t03(((

--- sprint04/t03_weather_forecast/js/script.js
+++ sprint04/t03_weather_forecast/js/script.js
@@ -1,46 +1,37 @@
-let key = '71e2d79e00d51250e656f69a1a4e9e25';
-let latitude = 49.988358;
-let longitude = 36.232845;
-let api = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=daily&appid=${key}`;
+let key = 'f5ff64b37a1e5727c406056b42a4aa4d';
+let latitude = 49.98;
+let longitude = 36.23;
+let api = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=daily&appid=${key}`;
 
-function main() {
-  fetch(api)
+fetch(api)
     .then(function(resp) {
-      return resp.json();
+        return resp.json();
     })
     .then(function(data) {
-      setData(data);
+        setData(data);
     })
-    // .catch(function(error) {
-    //   console.log(error);
-    // });
-}
+    .catch(function() {
+
+    })
 
 function setData(data) {
-  let array = data.dt;  
-  let img = document.getElementsByTagName('img');
-  let main = document.querySelector('.weather-table');
-  let days = main.getElementsByClassName('day');
-  for (let day = 0; day < days.length; day++) {
-    let date = new Date(array[day].dt * 1000);
-    let formattedDate =
-      date.getDate() +
-      '.' +
-      (date.getMonth() + 1 < 10
-        ? '0'.concat('', date.getMonth() + 1)
-        : date.getMonth() + 1);
-    days[day].querySelector('.data').innerHTML = formattedDate;
-    let temp =
-      (array[day].temp < 0 ? '-' : '+') +
-      Math.abs(array[day].temp) +
-      '&#176';
-    days[day].querySelector('.temp').innerHTML = temp;
-    let result =
-      'http://openweather.org/img/w/' + array[day].weather[0].icon + '.png';
-    img[day].src = result;
-  }
+    document.querySelector('.city').textContent = data.timezone;
+    let weatherTable = document.querySelector('.weather-table');
+    for (let i = 0; i < data.daily.length; i++) {
+        weatherTable.innerHTML +=
+            '<div class="content">' +
+            '<h3 class="data">' + trueData(data.daily[i]) + '</h3>' +
+            '<img src="https://openweathermap.org/img/wn/' + data.daily[i].weather[0].icon + '@2x.png" alt="#"></img>' +
+            '<p class="temp">' + (Math.round(data.daily[i].temp.day) - 273) + '&deg;</p>' +
+            '</div>';
+    }
 }
 
-window.onload = function() {
-  main();
-};
+function trueData(data) {
+    let date = new Date(+data.dt * 1000);
+    let options = {
+        month: 'numeric',
+        day: 'numeric',
+    }
+    return date.toLocaleDateString('ru', options);
+}
