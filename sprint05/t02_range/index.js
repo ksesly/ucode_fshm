5cd0dfadfd07d890cd168cdae01526db5a0e758d
ksesly <ks.you.04@gmail.com>
2023-09-06 00:25:25 +0300

    t02

---
+++ sprint05/t02_range/index.js
@@ -0,0 +1,22 @@
+module.exports = { checkDivision(a = 1, b = 60) {
+        while(a <= b) {
+            let divString = "";
+            let divNumbers = [];
+            if (a % 2 === 0 ) 
+                divNumbers.push('2');
+            if (a % 3 === 0 ) 
+                divNumbers.push('3');
+            if (a % 10 === 0 ) 
+                divNumbers.push('10');
+            
+            if (divNumbers.length > 0) {
+                divString += " is devided by ";
+                divString +=  divNumbers.join(", is divisible by ");
+            }
+            else 
+                divString += " -";
+            console.log("The number " + a + divString);
+            a++;
+        }
+    }
+}
