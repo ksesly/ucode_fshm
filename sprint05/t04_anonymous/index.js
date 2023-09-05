7801e4db338569ef5d57f322f721aa07b5b7b2fb
ksesly <ks.you.04@gmail.com>
2023-09-06 01:08:02 +0300

    t04

---
+++ sprint05/t04_anonymous/index.js
@@ -0,0 +1,12 @@
+module.exports = class Tutu {
+    constructor(name, alias, affiliation) {
+        this.name = name;
+        this.alias = alias;
+        this.affiliation = affiliation;
+    }
+    static getAnonymous(name, alias, affiliation) {
+        return new Tutu(name, alias, affiliation);
+    }
+} 
+
+
