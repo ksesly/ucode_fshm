f6e0f4df3b0a064630710924b518d2125aadd9f1
ksesly <ks.you.04@gmail.com>
2023-09-12 13:55:36 +0300

    ff

--- sprint06/t08_info/index.js
+++ sprint06/t08_info/index.js
@@ -4,25 +4,25 @@ const url = require('url');
 const server = http.createServer((req, res) => {
     const parsedUrl = url.parse(req.url, true);
 
-    const scriptFileName = __filename;
-    const scriptArguments = process.argv.slice(2);
-    const serverIpAddress = req.connection.remoteAddress;
-    const hostName = req.headers.host;
-    const protocolName = req.protocol || 'HTTP';
-    const queryMethod = req.method;
-    const userAgent = req.headers['user-agent'];
-    const clientIpAddress = req.connection.remoteAddress;
-    const queryParams = parsedUrl.query;
+    // const scriptFileName = __filename;
+    // const scriptArguments = process.argv.slice(2);
+    // const serverIpAddress = req.connection.remoteAddress;
+    // const hostName = req.headers.host;
+    // const protocolName = req.protocol || 'HTTP';
+    // const queryMethod = req.method;
+    // const userAgent = req.headers['user-agent'];
+    // const clientIpAddress = req.connection.remoteAddress;
+    // const queryParams = parsedUrl.query;
 
-    console.log('Name of executed script:', scriptFileName);
-    console.log('Arguments passed to the script:', scriptArguments);
-    console.log('IP address of the server:', serverIpAddress);
-    console.log('Host that invokes the current script:', hostName);
-    console.log('Name and version of the information protocol:', protocolName);
-    console.log('Query method:', queryMethod);
-    console.log('User-Agent information:', userAgent);
-    console.log('IP address of the client:', clientIpAddress);
-    console.log('List of parameters passed by URL:', queryParams);
+    console.log('Name of executed script:', __filename);
+    console.log('Arguments passed to the script:', process.argv.slice(2));
+    console.log('IP address of the server:', req.connection.remoteAddress);
+    console.log('Host that invokes the current script:', req.headers.host);
+    console.log('Name and version of the information protocol:', req.protocol || 'HTTP');
+    console.log('Query method:', req.method);
+    console.log('User-Agent information:', req.headers['user-agent']);
+    console.log('IP address of the client:', req.connection.remoteAddress);
+    console.log('List of parameters passed by URL:', parsedUrl.query);
 
     res.end('Check the console for the information.');
 });
