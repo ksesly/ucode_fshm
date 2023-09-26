436dcb7e2793d4015b82487c3326adf9e075fa31
ksesly <ks.you.04@gmail.com>
2023-09-26 11:01:20 +0300

    .

--- sprint07/t01_hack_it/index.js
+++ sprint07/t01_hack_it/index.js
@@ -17,64 +17,15 @@ app.use(session({
     saveUninitialized: true
 }));
 
-// app.get('/', (req, res) => {
-//     const data = {
-//         hashedPassword: req.session.hashedPassword || null,
-//         message: req.session.message || null,
-//         color: req.session.messageColor || 'black'
-//     };
-
-//     fs.readFile(__dirname + '/index.html', 'utf8', (err, htmlData) => {
-//         if (err) {
-//             console.error(err);
-//             res.status(500).send('Error reading the HTML file.');
-//         } else {
-//             htmlData = htmlData.replace('{{hashedPassword}}', data.hashedPassword || '');
-//             htmlData = htmlData.replace('{{messageColor}}', data.color || 'black');
-//             htmlData = htmlData.replace('{{message}}', data.message || '');
-//             res.send(htmlData);
-//         }
-//     });
-// });
-
-// app.post('/save-password', (req, res) => {
-//     const password = req.body.password;
-//     const salt = req.body.salt;
-
-//     // Hash the password with salt
-//     const hashedPassword = crypto.createHash('sha256').update(password + salt).digest('hex');
-
-//     // Store the hashed password in the session
-//     req.session.hashedPassword = hashedPassword;
-//     req.session.message = null;
-//     req.session.messageColor = 'black';
-
-//     res.redirect('/');
-// });
-
-// app.post('/guess-password', (req, res) => {
-//     const guess = req.body.guess;
-//     const storedHashedPassword = req.session.hashedPassword;
-
-//     // Hash the guessed password with the same salt
-//     const guessedHashedPassword = crypto.createHash('sha256').update(guess + req.body.salt).digest('hex');
-
-//     if (guessedHashedPassword === storedHashedPassword) {
-//         req.session.message = 'Hacked!';
-//         req.session.messageColor = 'green';
-//         req.session.hashedPassword = null; // Clear the hashed password
-//     } else {
-//         req.session.message = 'Access denied!';
-//         req.session.messageColor = 'red';
-//     }
-
-//     res.redirect('/');
-// });
 
 app.post('/clear-session', (req, res) => {
     req.session.destroy(() => {
         res.redirect('/');
     });
+});
+
+app.get('/', (req, resp) => {
+    resp.sendFile(__dirname + '/index.html')
 });
 
 app.listen(PORT, () => {
